8302c4e17aab8c059c2b052d2f1e72c644e128a5
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-03-15 19:30:01 +0100

    Messing about with receiving input, the scanf call causes it not to work so I must be doing something wrong..

--- YARRtificial_Intelligence.c
+++ YARRtificial_Intelligence.c
@@ -1,16 +1,30 @@
 
 #include <stdio.h>
+#include <fcntl.h>
 
 int main(void)
 {
 	int color_count, chip_count, grid_size, win_length, id;
 	float timeout;
 	scanf(" init %d %d %d %d %f %d", &color_count, &chip_count, &grid_size, &win_length, &timeout, &id);
-
 	printf("color 0\n");
+
+	int turn_count = 0;
 	int gravity = 0;
+	int cell_count = -1;
+
+	int q;
+	int r;
+	int s;
+	int value;
 	while(1)
 	{
+		printf("fetch cells\n");
+		// scanf(" cell_count %d", &cell_count);
+		// for (int i = 0; i < cell_count; i++)
+		// {
+		// 	scanf(" cell %d %d %d %d", &q, &r, &s, &value);
+		// }
 		printf("rotate %d\n", gravity);
 		gravity++;
 		gravity = gravity % 6;
